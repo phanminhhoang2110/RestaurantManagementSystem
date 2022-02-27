4a94ccde0b57639c87079ba13bb643cf33997894
Phan Minh Hoang <phanminhhoang2110@gmail.com>
2022-02-27 12:41:52 +0700

    init commit

---
+++ database/migrations/2022_01_06_170036_tables.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
