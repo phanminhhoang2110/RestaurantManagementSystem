eaeca1d8f0d0a14d6519ac99dd0751ab7f6eec2a
Phan Minh Hoang <phanminhhoang2110@gmail.com>
2022-02-27 16:38:46 +0700

    Setup basic auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('password');
             $table->int('birthday');
-            $table->timestamp();
+            $table->timestamps();
             $table->softDeletes();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::get('register', 'App\Http\Controllers\AuthenticationController@register');
+    Route::post('login', 'App\Http\Controllers\AuthenticationController@login');
+    Route::post('logout', 'AuthenticationController@logout');
+    Route::post('refresh', 'AuthenticationController@refresh');
+    Route::post('me', 'AuthenticationController@me');
+
 });
 
 Route::get('check', 'App\Http\Controllers\TestController@index');
